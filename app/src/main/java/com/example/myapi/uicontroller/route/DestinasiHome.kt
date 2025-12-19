ea7b9d0977f88466a2f59ad9a5e7cae311122647
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-12-19 14:46:56 +0700

    Menambahkan DestinasiHome sebagai rute halaman utama aplikasi

---
+++ app/src/main/java/com/example/myapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myapi.uicontroller.route
+
+import com.example.myapi.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
