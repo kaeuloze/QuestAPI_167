15f14436a5b2cb21570ac337dc0032aaf51654a1
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-12-26 22:27:24 +0700

    Menambahkan DetailViewModel untuk mengelola status UI detail siswa berdasarkan ID dari navigasi.

--- app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/DetailViewModel.kt
@@ -1,9 +1,20 @@
 package com.example.myapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.myapi.modeldata.DataSiswa
+import com.example.myapi.repositori.RepositoryDataSiswa
+import com.example.myapi.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail{
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+class DetailViewModel (savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
 }
